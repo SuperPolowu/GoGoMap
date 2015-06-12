b4e4998b7a862cd9dc4064726e7a5a52bcbe06c0
polo.wu <polo.wu@emelexplorer.com>
2015-06-12 10:45:21 +0800

    Initial Commit

---
+++ GoGoMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoGoMap
+//
+//  Created by PoloEmel on 2015/6/12.
+//  Copyright (c) 2015年 Polo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoGoMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoGoMap
+//
+//  Created by PoloEmel on 2015/6/12.
+//  Copyright (c) 2015年 Polo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
